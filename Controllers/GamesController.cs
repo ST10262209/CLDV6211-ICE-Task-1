2035bd23fd62b58b81961b567b0f4a54a56a9779
Cade Gamble <ST10262209@vcconnect.edu.za>
2024-03-22 12:39:41 +0200

    Initial commit

---
+++ Controllers/GamesController.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MvcGame.Data;
+using MvcGame.Models;
+
+namespace MvcGame.Controllers
+{
+    public class GamesController : Controller
+    {
+        private readonly MvcGameContext _context;
+
+        public GamesController(MvcGameContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Games
+        // GET: Movies
+        public async Task<IActionResult> Index(string gameGenre, string searchString)
+        {
+            if (_context.Game == null)
+            {
+                return Problem("Entity set 'MvcGameContext.Game'  is null.");
+            }
+
+            // Use LINQ to get list of genres.
+            IQueryable<string> genreQuery = from m in _context.Game
+                                            orderby m.Genre
+                                            select m.Genre;
+            var movies = from m in _context.Game
+                         select m;
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title!.Contains(searchString));
+            }
+
+            if (!string.IsNullOrEmpty(gameGenre))
+            {
+                movies = movies.Where(x => x.Genre == gameGenre);
+            }
+
+            var movieGenreVM = new GameGenreViewModel
+            {
+                Genres = new SelectList(await genreQuery.Distinct().ToListAsync()),
+                Games = await movies.ToListAsync()
+            };
+
+            return View(movieGenreVM);
+        }
+
+        // GET: Games/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var game = await _context.Game
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (game == null)
+            {
+                return NotFound();
+            }
+
+            return View(game);
+        }
+
+        // GET: Games/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Games/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,ReleaseDate,Genre,Price,Rating")] Game game)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(game);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(game);
+        }
+
+        // GET: Games/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var game = await _context.Game.FindAsync(id);
+            if (game == null)
+            {
+                return NotFound();
+            }
+            return View(game);
+        }
+
+        // POST: Games/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ReleaseDate,Genre,Price,Rating")] Game game)
+        {
+            if (id != game.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(game);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!GameExists(game.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(game);
+        }
+
+        // GET: Games/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var game = await _context.Game
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (game == null)
+            {
+                return NotFound();
+            }
+
+            return View(game);
+        }
+
+        // POST: Games/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var game = await _context.Game.FindAsync(id);
+            if (game != null)
+            {
+                _context.Game.Remove(game);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool GameExists(int id)
+        {
+            return _context.Game.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Models/SeedData.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcGame.Data;
+using MvcGame.Models;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new MvcGameContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<MvcGameContext>>()))
+        {
+            // Look for any movies.
+            if (context.Game.Any())
+            {
+                return;   // DB has been seeded
+            }
+            context.Game.AddRange(
+                new Game
+                {
+                    Title = "Destiny 2",
+                    Rating = "R",
+                    ReleaseDate = DateTime.Parse("2017-9-6"),
+                    Genre = "FPS and Action MMO",
+                    Price = 1200M
+
+                },
+                new Game
+                {
+                    Title = "Genshin Impact",
+                    Rating = "G",
+                    ReleaseDate = DateTime.Parse("2020-9-28"),
+                    Genre = "Action Role-Playing Game",
+                    Price = 0M
+                },
+                new Game
+                {
+                    Title = "Honkai Star Rail",
+                    Rating = "G",
+                    ReleaseDate = DateTime.Parse("2023-4-26"),
+                    Genre = "Action Role-Playing Game",
+                    Price = 0M
+                },
+                new Game
+                {
+                    Title = "Baldurs Gate 3",
+                    Rating = "NC-17",
+                    ReleaseDate = DateTime.Parse("2023-8-3"),
+                    Genre = "Role-Playing Game",
+                    Price = 800M
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
